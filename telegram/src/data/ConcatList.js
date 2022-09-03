9f5bfe337846e221cc81d56b25c10a8183d7617e
batuctn <batuctnn14@gmail.com>
2022-09-03 05:23:33 +0300

    mock data add for contact list

---
+++ telegram/src/data/ConcatList.js
@@ -0,0 +1,31 @@
+const chatData = [
+  {
+    id: 1,
+    firstName: 'Cem',
+    lastName: 'Eriş',
+    image: require('../assets/1.jpg'),
+    lastSeen: 'last seen 19 min ago',
+  },
+  {
+    id: 2,
+    firstName: 'Kaan',
+    lastName: 'Can',
+    image: require('../assets/2.jpg'),
+    lastSeen: 'last seen 55 min ago',
+  },
+  {
+    id: 3,
+    firstName: 'Ertuğrul Gazi',
+    lastName: 'Yıldız',
+    image: require('../assets/3.jpg'),
+    lastSeen: 'last seen 1 hour ago',
+  },
+  {
+    id: 4,
+    firstName: 'Yunus Emre',
+    lastName: 'Çıracı',
+    image: require('../assets/4.jpg'),
+    lastSeen: 'last seen 3 hours ago',
+  },
+];
+export default chatData;
